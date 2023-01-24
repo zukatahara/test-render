4e08c5a118ede4802aadb7be947b3e37a245dd73
unknown <quyenpro77a@gmail.com>
2023-01-24 20:26:28 +0700

    abc

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
-const express =require('express')
+const express = require("express");
 const app = express();
 
-
-app.get("/",(req,res)=>{
-    res.send(`hello`)
-})
-app.listen(3000, ()=> console.log(`running on port 3000`));
+app.get("/", (req, res) => {
+  res.send(`hello`);
+});
+app.get("/abc", (req, res) => {
+  res.send("abc page");
+});
+app.listen(3000, () => console.log(`running on port 3000`));
