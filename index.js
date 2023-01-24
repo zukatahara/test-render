8b520a84e6affe3605af5dba88cb279bc6862d6d
unknown <quyenpro77a@gmail.com>
2023-01-24 20:38:02 +0700

    abc

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 const express = require("express");
+const ytdl = require("ytdl-core");
 const app = express();
 
 app.get("/", (req, res) => {
   res.send(`hello`);
 });
 app.get("/abc", (req, res) => {
+  const url = req.query.url
+    ? req.query.url
+    : "https://www.youtube.com/embed/wz-ZkPEGyeo?start=10&end=15";
+  ytdl(url, { filter: "audioonly" }).pipe(
+    fs.createWriteStream(`${Date.now()}_video.mp3`)
+  );
   res.send("abc page");
 });
 app.listen(3000, () => console.log(`running on port 3000`));
