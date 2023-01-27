049bdbbefd566265574bf9569b5b7be86c316bd4
unknown <quyenpro7a@gmail.com>
2023-01-27 19:00:50 +0700

    abc

--- index.js
+++ index.js
@@ -4,18 +4,19 @@ const app = express();
 const fs = require("fs");
 const path = require("path");
 var downloadFolder = path.join(process.env.USERPROFILE, "/Downloads/");
-console.log("downloadFolder", downloadFolder);
-
+console.log("downloadFolder", process.env.USERPROFILE);
+console.log(`aaa`);
 app.get("/", (req, res) => {
   res.send(`hello`);
 });
 app.get("/abc", async (req, res) => {
-  const url = req.query.url
-    ? req.query.url
-    : "https://www.youtube.com/embed/wz-ZkPEGyeo?start=10&end=15";
-  ytdl(url, { filter: "audioonly" }).pipe(
-    await fs.createWriteStream(`${downloadFolder}${Date.now()}_video.mp3`)
-  );
-  res.send("abc page");
+  res.sendFile("1674820418143_video.mp3", { root: __dirname });
+  // const url = req.query.url
+  //   ? req.query.url
+  //   : "https://www.youtube.com/embed/wz-ZkPEGyeo?start=10&end=15";
+  // ytdl(url, { filter: "audioonly" }).pipe(
+  //   await fs.createWriteStream(`${Date.now()}_video.mp3`)
+  // );
+  // res.send("abc page");
 });
 app.listen(3000, () => console.log(`running on port 3000`));
